748102a7be9cd512460e03d4eee4fada4dcb5be9
Placide <placidekad@gmail.com>
2026-01-18 08:43:10 +0200

    clearer output

--- main.c
+++ main.c
@@ -10,31 +10,28 @@ int main(int argc, char* argv[]){
   if(argc > 1){
     file_path=argv[1];
     struct stat file_infos;
-    char* command_result;
     if(stat(file_path,&file_infos)==0){
-      int permissions=file_infos.st_mode & 0777;
+      char file_type_bit='-';
+      char* permissions_bits;
+      int permissions=(int)file_infos.st_mode & 0777;
+      long int file_size=file_infos.st_size;
       struct group* grp=getgrgid(file_infos.st_gid);
       struct passwd* pwd=getpwuid(file_infos.st_uid);
-      printf("group: %s\n",grp->gr_name);
-      printf("user : %s\n",pwd->pw_name);
-      printf("%o\n",permissions);
-      printf("%ldB\n",file_infos.st_size);
+
       if(S_ISREG(file_infos.st_mode)){
         struct stat link_infos;
         if(lstat(file_path,&link_infos)==0){
           if(S_ISLNK(link_infos.st_mode)){
-            printf("l\n");
-            printf("%o\n",link_infos.st_mode&0777);
-          }
-          else{
-            printf("f\n");
+            file_type_bit='l';
+            permissions=(int)link_infos.st_mode & 0777;
           }
         }
       }else if(S_ISDIR(file_infos.st_mode)){
-        printf("d\n");
+        file_type_bit='d';
       }else{
         return EXIT_FAILURE;
       }
+      printf("%c%o \t %s \t %s \t %ldB\n",file_type_bit,permissions,pwd->pw_name,grp->gr_name,file_size);
     }else{
       printf("Error: fspector: can't find %s\n",file_path);
       return EXIT_FAILURE;
