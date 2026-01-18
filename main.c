18533b60a2d4fb5750c5adbd1487e6aa2fd72b20
Placide <placidekad@gmail.com>
2026-01-18 16:05:20 +0200

    open directory and display it's content

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <sys/stat.h>
 #include <pwd.h>
 #include <grp.h>
+#include <dirent.h>
 #include "octal_permission_converter.h"
 
 int main(int argc, char* argv[]){
@@ -11,32 +12,47 @@ int main(int argc, char* argv[]){
   if(argc > 1){
     file_path=argv[1];
     struct stat file_infos;
+
     if(stat(file_path,&file_infos)==0){
-      char file_type_bit='-';
-      char* permissions_bits;
-      int permissions=file_infos.st_mode & 0777;
-      permissions_bits=converter(permissions);
-      long int file_size=file_infos.st_size;
-      struct group* grp=getgrgid(file_infos.st_gid);
-      struct passwd* pwd=getpwuid(file_infos.st_uid);
 
       if(S_ISREG(file_infos.st_mode)){
+
+        char file_type_bit='-';
+        int permissions=file_infos.st_mode & 0777;
+        char* permissions_bits=converter(permissions);
+        long int file_size=file_infos.st_size;
+        struct group* grp=getgrgid(file_infos.st_gid);
+        struct passwd* pwd=getpwuid(file_infos.st_uid);
         struct stat link_infos;
+
         if(lstat(file_path,&link_infos)==0){
+
           if(S_ISLNK(link_infos.st_mode)){
+
             file_type_bit='l';
             permissions=link_infos.st_mode & 0777;
             printf("%d\n",permissions);
             permissions_bits=converter(permissions);
           }
         }
+        printf("%c%s %s %s %ldB\n",file_type_bit,permissions_bits,pwd->pw_name,grp->gr_name,file_size);
+        free(permissions_bits);
       }else if(S_ISDIR(file_infos.st_mode)){
-        file_type_bit='d';
+
+        struct dirent* entry;
+        DIR *dp=opendir(file_path);
+        if (dp==NULL){
+          perror("Could not open the directory\n");
+          return EXIT_FAILURE;
+        }
+
+        while((entry=readdir(dp))!=NULL){
+          printf("%s\n",entry->d_name);
+        }
+        closedir(dp);
       }else{
         return EXIT_FAILURE;
       }
-      printf("%c%s \t %s \t %s \t %ldB\n",file_type_bit,permissions_bits,pwd->pw_name,grp->gr_name,file_size);
-      free(permissions_bits);
     }else{
       printf("Error: fspector: can't find %s\n",file_path);
       return EXIT_FAILURE;
